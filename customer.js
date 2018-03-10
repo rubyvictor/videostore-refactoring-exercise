6e883546804de3a24cf1f2c5ac62e0a72fe8b070
Victor Lee <myleevictor@gmail.com>
2018-03-10 15:33:52 +0800

    removed frequent renter points from Customer model

--- customer.js
+++ customer.js
@@ -2,11 +2,6 @@ class Customer {
   constructor({ name, rentals }) {
     this.name = name;
     this.rentals = rentals;
-    this.frequentRenterPoints = 0;
-  }
-
-  incrementRentalPoints() {
-    this.frequentRenterPoints++;
   }
 }
 
