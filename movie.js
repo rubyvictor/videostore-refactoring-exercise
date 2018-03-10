52c118fc77608c9393b1a34584c71cef615580b7
Victor Lee <myleevictor@gmail.com>
2018-03-10 16:36:51 +0800

    introduced Rental Class to rental_calculator

--- movie.js
+++ movie.js
@@ -1,5 +1,3 @@
-import { mod } from "../Library/Caches/typescript/2.6/node_modules/@types/prelude-ls";
-
 class Movie {
   constructor({ id, title, code }) {
     this.id = id;
@@ -7,46 +5,5 @@ class Movie {
     this.code = code;
   }
 }
-class NewMovie extends Movie {
-  constructor({id,title}){
-    super({id,title,code:"new"});
-  }
 
-  getBillForRental(days){
-    return days * 3;
-  }
-}
-
-class RegularMovie extends Movie {
-  constructor({id,title}){
-    super({id,title,code:"regular"});
-  }
-
-  getBillForRental(days){
-    let bill = 2;
-        if (days > 2) {
-          bill += (days - 2) * 1.5;
-  }
-  return bill;
-}
-
-class ChildrenMovie extends Movie {
-  constructor({id,title}){
-    super({id,title,code:"children"})
-  }
-
-  getBillForRental(days){
-    let bill = 3;
-    if (days > 3){
-      bill += (days -3) * 1.5
-    }
-  }
-}
-
-
-module.exports = {
-  Movie,
-  NewMovie,
-  RegularMovie,
-  ChildrenMovie
-}
+module.exports = Movie;
